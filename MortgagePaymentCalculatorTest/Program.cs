6b8409ea1e3e11d176c3ecbdbf2b69d6b42a943c
Steven Elliott <steven.peter.elliott@gmail.com>
2016-02-18 22:10:05 -0800

    working on tests

--- MortgagePaymentCalculatorTest/Program.cs
+++ MortgagePaymentCalculatorTest/Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
 
 namespace MortgagePaymentCalculatorTest
 {
-    class Program
+    [TestFixture]
+    public class MortgagePaymentCalculatorTests
     {
-        static void Main(string[] args)
+        public IWebDriver Driver { get; private set; }
+
+        [SetUp]
+        public virtual void Setup()
         {
+
+            var options = new ChromeOptions();
+            options.AddArgument("--disable-extensions");
+            Driver = new ChromeDriver(options);
+           
         }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void ShouldClickOnLoans()
+        {
+
+            Driver.Navigate().GoToUrl("http://ia.ca");
+            Driver.Manage().Window.Maximize();
+
+            var waitForEnglish = new WebDriverWait(Driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementExists((By.CssSelector(".test:contains('Loans')"))));
+
+
+            Driver.FindElement(By.CssSelector(".test")).Click();
+            Driver.FindElement(By.CssSelector("a[href*='/mortgages']")).Click();
+
+            Thread.Sleep(5000);
+
+        }
+
+        public static void Main(string[] args)
+        {
+
+        }
+
     }
+
 }
---
+++ MortgagePaymentCalculatorTest/SeleniumHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace MortgagePaymentCalculatorTest
+{
+    public abstract class SeleniumHelpers
+    {
+        protected readonly IWebDriver driver;
+
+        protected SeleniumHelpers(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        protected IWebElement Find(By by)
+        {
+            return driver.FindElement(by);
+        }
+
+        public bool Enabled(IWebElement element)
+        {
+            return element.Enabled;
+        }
+
+        private bool VisibleAndEnabled(IWebElement element)
+        {
+            return element.Displayed && Enabled(element);
+        }
+        
+        protected IWebElement WaitUntilAvailable(By by)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(120));
+            //wait.Until(x => VisibleAndEnabled(x.FindElement(by)));
+
+            return Find(by);
+        }
+    }
+}
